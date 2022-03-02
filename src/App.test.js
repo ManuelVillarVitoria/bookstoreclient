4c754e57920a77befe6e1fcf77e217e60e73e7ec
Manu <manu.villar.vitoria@gmail.com>
2022-03-02 17:53:13 +0100

    CI-CD setup and deply to heroku

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from "@testing-library/react"
+import App from './App';
+
+
+describe('App component', () => {
+    it ('should render app with error', () => {
+        const {asFragment } = render(<App />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
